cf9892cd70cb72af077d6b57d4c8ae66116039a8
U4rn4 <arnau.serra@gmail.com>
2024-03-29 02:02:12 +0100

    a

--- PRO2/parcialmock/Ex2/mergeIntoFirstList.cc
+++ PRO2/parcialmock/Ex2/mergeIntoFirstList.cc
@@ -6,24 +6,23 @@
 void mergeIntoFirstList(list<int> &l1, list<int> l2){
     list<int>::iterator it1=l1.begin();
     list<int>::iterator it2=l2.begin();
-    while (it2!=l2.end())
+    while (it2!=l2.end() and it1!=l1.end())
     {
-        if (*it1 > *it2)
+        if (*it1 <= *it2)
         {
             ++it1;
         }
-        else if (*it1<=*it2)
+        else if (*it1>*it2)
         {
             l1.insert(it1,*it2);
             ++it2;
         }
-        else if (it1==l1.end())
-        {
-            l1.splice(it1, l2);
-            it2 = l1.end();
-        }
-        
         
     }
     
+    while (it2!=l2.end())
+    {
+        l1.insert(it1, *it2);
+        ++it2;
+    }
 }
