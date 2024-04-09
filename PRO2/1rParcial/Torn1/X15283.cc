1460e6e3faec9bdb05327d1b80355368029b7008
U4rn4 <arnau.serra@gmail.com>
2024-04-09 18:57:23 +0200

    X15283 a mi manera

--- PRO2/1rParcial/Torn1/X15283.cc
+++ PRO2/1rParcial/Torn1/X15283.cc
@@ -1,96 +1,57 @@
-/*  
-    Este programa fue hecho por Fyodor.
-    Su github:
-    https://github.com/fyodormeteor
-*/
-
 #include <iostream>
-#include <list>
-#include <string>
 #include <stack>
-
+#include <string>
 using namespace std;
 
-// Uses an auxiliary stack to reverse the original stack, and uses the auxiliary stack to output the contents of the original stack in reversed order.
-void reverseAndPrintStack(stack<char>& s)
-{
-    stack<char> s_aux;
+int main(){
+    string str;
+    stack<char> s;
 
-    while (!s.empty())
+    while (cin >> str)
     {
-        char content = s.top();
-        s_aux.push(content);
-        s.pop();
-    }
-
-    while (!s_aux.empty())
-    {
-        cout << s_aux.top();
-        s_aux.pop();
+        for (int i = 0; i < str.length(); i++)
+        {
+            if (s.empty() and (str[i]==')' or str[i]==']'))
+            {
+                s.push(str[i]);
+            }
+            else if (str[i]=='(' or str[i]=='[')
+            {
+                s.push(str[i]);
+            }
+            else if (str[i]==')' and s.top()=='(')
+            {
+                s.pop();
+            }
+            else if (str[i]==']' and s.top()=='[')
+            {
+                s.pop();
+            }
+            else if (str[i]==']' and s.top()=='(')
+            {
+                s.pop();
+                --i;
+            }
+            else s.push(str[i]);
+        }
+        
+        if(s.empty()) cout << endl;
+        else
+        {
+            stack<char> s2;
+            while (not s.empty())
+            {
+                s2.push(s.top());
+                s.pop();
+            }
+            while (not s2.empty())
+            {
+                cout << s2.top();
+                s2.pop();
+            }
+            cout << endl;
+        }
+        
     }
     
-    cout << endl;
-}
-
-int main()
-{
-    string input_line;
-    while(getline(cin, input_line))
-    {
-        list<char> input; 
-        for(int i = 0; i < input_line.size(); ++i) // Convert the read string into a list of characters
-        {
-            input.push_back(input_line[i]);
-        }
-
-        stack<char> s;
-        char c;
-        auto it = input.begin();
-        while(it != input.end()) // Iterate on the list of characters
-        {
-            c = *it;
-            
-            if (c == '(' or c == '[')
-            {
-                s.push(c);
-            }
-
-            if (c == ')')
-            {
-                if (!s.empty() and s.top() == '(')
-                {
-                    s.pop();
-                }
-                else
-                {
-                    s.push(c);
-                }
-            }
-
-            if (c == ']')
-            {
-                if (!s.empty() and s.top() == '[')
-                {
-                    s.pop();
-                }
-                else if (!s.empty() and s.top() == '(') // delete the element from the stack and add ']' to the next position of the list
-                                                        // this way, the newly added ']' will be treated in the next iteration.
-                {
-                    s.pop();
-                    auto it_aux = it;
-                    it_aux++;
-                    input.insert(it_aux, ']');
-                }
-                else
-                {
-                    s.push(c);
-                }
-            }
-
-            it++;
-        }
-
-        // Any elements that could not be reduce will be left in the stack, thus we print the stack (in reverse order)
-        reverseAndPrintStack(s);
-    }
 }
