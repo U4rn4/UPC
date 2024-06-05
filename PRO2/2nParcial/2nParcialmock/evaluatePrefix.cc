a029f249d75bc72c90c096b27fe66b4ea5f9803e
U4rn4 <arnau.serra@gmail.com>
2024-06-05 20:15:05 +0200

    eficiencia mock 2 ej 5

--- PRO2/2nParcial/2nParcialmock/evaluatePrefix.cc
+++ PRO2/2nParcial/2nParcialmock/evaluatePrefix.cc
@@ -1,43 +1,40 @@
 #include "evaluatePrefix.hh"
-#include <string>
 
+#include <algorithm>
 
-string aux(const BinTree<string> t, const int n, int a){
-    if (t.value()=="Concat")
-    {
-        string s = aux(t.left(),n,a);
-        if(s.length()<n or a!=0){
-            string s2 = aux(t.right(),n,a);
-            return s + s2;
-        } 
-        return s;
+string evaluatePrefix(BinTree<string> t, int n, bool rev) {
+  if (n <= 0)
+    return "";
+  else if (t.value() == "Concat") {
+    if (not rev) {
+      string s1, s2;
+      s1 = evaluatePrefix(t.left(), n, false);
+      s2 = evaluatePrefix(t.right(), n - s1.size(), false);
+      return s1 + s2;
+    } else {
+      string s1, s2;
+      s2 = evaluatePrefix(t.right(), n, true);
+      s1 = evaluatePrefix(t.left(), n - s2.size(), true);
+      return s1 + s2;
     }
-    if (t.value()=="Reverse")
-    {
-        string s=aux(t.left(),n,a+1);
-        string f;
-        for (int i = s.length()-1; i >= 0; i--)
-        {
-            f+=s[i];
-        }
-
-        return f;
+  } else if (t.value() == "Reverse") {
+    string s1;
+    s1 = evaluatePrefix(t.left(), n, not rev);
+    reverse(s1.begin(), s1.end());
+    return s1;
+  } else {
+    string s = t.value();
+    if (n > s.size())
+      return s;
+    else if (not rev)
+      return s.substr(0, n);
+    else {
+      return s.substr(s.size()-n, n);
     }
-
-    return t.value();    
+  }
 }
 
-// Pre:  t és un arbre no buit que representa una expressió correcta
-//       sobre strings de lletres minúscules i els operadors Concat, Reverse.
-//       n>=0
-// Post: Retorna el prefix de longitut n de l'avaluació de l'expressió representada per t.
-//       En cas que n sigui més gran que la longitut d'aquesta avaluació,
-//       llavors retorna només l'avaluació, cap caràcter més.
-string evaluatePrefix(BinTree<string> t, int n){
-    if (n==0)
-    {
-        return "";
-    }
-    return aux(t,n,0).substr(0,n);
+string evaluatePrefix(BinTree<string> t, int n) {
+  return evaluatePrefix(t,n,false);
 }
 
