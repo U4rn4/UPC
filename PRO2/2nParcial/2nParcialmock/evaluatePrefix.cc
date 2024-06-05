b504a7233a788fe925d1cd3fb91cbb357a627a7c
U4rn4 <arnau.serra@gmail.com>
2024-06-05 14:21:52 +0200

    mock y homework

---
+++ PRO2/2nParcial/2nParcialmock/evaluatePrefix.cc
@@ -0,0 +1,51 @@
+#include "evaluatePrefix.hh"
+#include <string>
+
+
+string aux(const BinTree<string> t, const int n, int a){
+    if (t.value()=="Concat")
+    {
+        string s2 = "";
+        string s = aux(t.left(),n,a);
+        if(s.length()<n or a!=0) s2 = aux(t.right(),n,a);
+        return s + s2;
+    }
+    if (t.value()=="Reverse")
+    {
+        string s=aux(t.left(),n,a+1);
+        string f;
+        if (a==0)
+        {
+            for (int i = s.length()-1; i >= s.length()-n; i--)
+            {
+                f+=s[i];
+            }
+        }
+        else
+        {
+            for (int i = s.length()-1; i >= 0; i--)
+            {
+                f+=s[i];
+            }
+        }
+         
+        return f;
+    }
+
+    return t.value();    
+}
+
+// Pre:  t és un arbre no buit que representa una expressió correcta
+//       sobre strings de lletres minúscules i els operadors Concat, Reverse.
+//       n>=0
+// Post: Retorna el prefix de longitut n de l'avaluació de l'expressió representada per t.
+//       En cas que n sigui més gran que la longitut d'aquesta avaluació,
+//       llavors retorna només l'avaluació, cap caràcter més.
+string evaluatePrefix(BinTree<string> t, int n){
+    if (n==0)
+    {
+        return "";
+    }
+    return aux(t,n,0).substr(0,n);
+}
+
